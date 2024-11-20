bb361fa10caef3e8031905b2cf91fe5ba02d1c80
Hao <s1810942008@gms.nutc.edu.tw>
2024-11-20 19:32:05 +0800

    add project

---
+++ RandomColor_ByAlpha/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace RandomColor_ByBrightness;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        buttonTransfer = new Button();
+        alphaTextBox = new TextBox();
+        label1 = new Label();
+        middleGrayButton = new Button();
+        resultLabel = new Label();
+        canvas = new Panel();
+        SuspendLayout();
+        // 
+        // buttonTransfer
+        // 
+        buttonTransfer.Location = new Point(93, 70);
+        buttonTransfer.Name = "buttonTransfer";
+        buttonTransfer.Size = new Size(75, 23);
+        buttonTransfer.TabIndex = 0;
+        buttonTransfer.Text = "轉換並複製";
+        buttonTransfer.UseVisualStyleBackColor = true;
+        buttonTransfer.Click += buttonTransfer_Click;
+        // 
+        // alphaTextBox
+        // 
+        alphaTextBox.Location = new Point(77, 41);
+        alphaTextBox.Name = "alphaTextBox";
+        alphaTextBox.Size = new Size(100, 23);
+        alphaTextBox.TabIndex = 1;
+        alphaTextBox.Text = "128";
+        alphaTextBox.TextAlign = HorizontalAlignment.Center;
+        alphaTextBox.TextChanged += alphaTextBox_TextChanged;
+        alphaTextBox.KeyPress += alphaTextBox_KeyPress;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(74, 9);
+        label1.Name = "label1";
+        label1.Size = new Size(103, 15);
+        label1.TabIndex = 2;
+        label1.Text = "輸入想要的透明度";
+        // 
+        // middleGrayButton
+        // 
+        middleGrayButton.Location = new Point(183, 12);
+        middleGrayButton.Name = "middleGrayButton";
+        middleGrayButton.Size = new Size(68, 23);
+        middleGrayButton.TabIndex = 3;
+        middleGrayButton.Text = "設定50%";
+        middleGrayButton.UseVisualStyleBackColor = true;
+        middleGrayButton.Click += middleGrayButton_Click;
+        // 
+        // resultLabel
+        // 
+        resultLabel.AutoSize = true;
+        resultLabel.Location = new Point(22, 152);
+        resultLabel.Name = "resultLabel";
+        resultLabel.Size = new Size(0, 15);
+        resultLabel.TabIndex = 4;
+        // 
+        // canvas
+        // 
+        canvas.Location = new Point(192, 61);
+        canvas.Name = "canvas";
+        canvas.Size = new Size(49, 52);
+        canvas.TabIndex = 5;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(253, 176);
+        Controls.Add(canvas);
+        Controls.Add(resultLabel);
+        Controls.Add(middleGrayButton);
+        Controls.Add(label1);
+        Controls.Add(alphaTextBox);
+        Controls.Add(buttonTransfer);
+        Name = "Form1";
+        Text = "Form1";
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private Button buttonTransfer;
+    private TextBox alphaTextBox;
+    private Label label1;
+    private Button middleGrayButton;
+    private Label resultLabel;
+    private Panel canvas;
+}
---
+++ RandomColor_ByAlpha/Form1.cs
@@ -0,0 +1,88 @@
+using System.Reflection.Emit;
+
+namespace RandomColor_ByBrightness;
+
+public partial class Form1 : Form
+{
+    private static readonly Random random = new Random();
+    private double _brightness;
+    private const double _tolerance = 0; // 亮度容忍範圍
+    private int R, G, B;
+
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    // KeyPress 限制輸入字元
+    private void alphaTextBox_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+        {
+            e.Handled = true; // 阻止非數字輸入
+        }
+    }
+
+    // TextChanged 確保內容合法
+    private void alphaTextBox_TextChanged(object sender, EventArgs e)
+    {
+        if (int.TryParse(alphaTextBox.Text, out int value))
+        {
+            if (value >= 256)
+            {
+                alphaTextBox.Text = "255"; // 自動修正為最大值
+                alphaTextBox.SelectionStart = alphaTextBox.Text.Length; // 將游標放到最後
+            }
+        }
+        else if (!string.IsNullOrEmpty(alphaTextBox.Text))
+        {
+            alphaTextBox.Text = string.Empty; // 非法輸入清空
+        }
+    }
+
+    private void middleGrayButton_Click(object sender, EventArgs e)
+    {
+        alphaTextBox.Text = "128";
+        int i = int.Parse(alphaTextBox.Text);
+        GetRGB(i);
+
+    }
+
+    private void buttonTransfer_Click(object sender, EventArgs e)
+    {
+        int i = int.Parse(alphaTextBox.Text);
+        GetRGB(i);
+    }
+
+    public void GetRGB(double _targetBrightness)
+    {
+        do
+        {
+            // 隨機生成 R、G、B 值
+            R = random.Next(0, 256);
+            G = random.Next(0, 256);
+            B = random.Next(0, 256);
+
+            // 計算亮度
+            _brightness = 0.299 * R + 0.587 * G + 0.114 * B;
+        } while (_brightness < _targetBrightness - _tolerance || _brightness > _targetBrightness + _tolerance);
+
+        // 顯示結果
+        Console.WriteLine($"Generated RGB: R={R}, G={G}, B={B}, Brightness={_brightness}");
+        resultLabel.Text = $"R={R}, G={G}, B={B}, Brightness={Math.Round(_brightness,1)}";
+
+        canvas.BackColor = Color.FromArgb(R, G, B);
+        // 創建 Bitmap 並繪製顏色
+        using (Bitmap bmp = new Bitmap(canvas.Width, canvas.Height))
+        {
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(canvas.BackColor); // 填充 Panel 的背景顏色
+            }
+
+            // 複製圖片到剪貼簿
+            Clipboard.SetImage(bmp);
+            //MessageBox.Show("顏色圖片已複製到剪貼簿！");
+        }
+    }
+}
