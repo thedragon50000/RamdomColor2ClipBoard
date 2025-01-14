6a2322c481fd523eaa41346d8fad66fdf3a929de
Hao <s1810942008@gms.nutc.edu.tw>
2025-01-14 14:24:15 +0800

    增加限定色相功能

--- RandomColor_ByAlpha/RandomColor.Designer.cs
+++ RandomColor_ByAlpha/RandomColor.Designer.cs
@@ -35,11 +35,19 @@ partial class RandomColor
         middleGrayButton = new Button();
         resultLabel = new Label();
         canvas = new Panel();
+        groupBox1 = new GroupBox();
+        radioButton1 = new RadioButton();
+        radioButton2 = new RadioButton();
+        radioButton3 = new RadioButton();
+        radioButton4 = new RadioButton();
+        radioButton5 = new RadioButton();
+        radioButton6 = new RadioButton();
+        groupBox1.SuspendLayout();
         SuspendLayout();
         // 
         // buttonTransfer
         // 
-        buttonTransfer.Location = new Point(93, 70);
+        buttonTransfer.Location = new Point(83, 70);
         buttonTransfer.Name = "buttonTransfer";
         buttonTransfer.Size = new Size(75, 23);
         buttonTransfer.TabIndex = 0;
@@ -49,7 +57,7 @@ partial class RandomColor
         // 
         // alphaTextBox
         // 
-        alphaTextBox.Location = new Point(77, 41);
+        alphaTextBox.Location = new Point(67, 41);
         alphaTextBox.Name = "alphaTextBox";
         alphaTextBox.Size = new Size(100, 23);
         alphaTextBox.TabIndex = 1;
@@ -61,7 +69,7 @@ partial class RandomColor
         // label1
         // 
         label1.AutoSize = true;
-        label1.Location = new Point(74, 9);
+        label1.Location = new Point(64, 9);
         label1.Name = "label1";
         label1.Size = new Size(103, 15);
         label1.TabIndex = 2;
@@ -69,7 +77,7 @@ partial class RandomColor
         // 
         // middleGrayButton
         // 
-        middleGrayButton.Location = new Point(183, 12);
+        middleGrayButton.Location = new Point(173, 12);
         middleGrayButton.Name = "middleGrayButton";
         middleGrayButton.Size = new Size(68, 23);
         middleGrayButton.TabIndex = 3;
@@ -80,23 +88,110 @@ partial class RandomColor
         // resultLabel
         // 
         resultLabel.AutoSize = true;
-        resultLabel.Location = new Point(22, 152);
+        resultLabel.Location = new Point(67, 143);
         resultLabel.Name = "resultLabel";
         resultLabel.Size = new Size(0, 15);
         resultLabel.TabIndex = 4;
         // 
         // canvas
         // 
-        canvas.Location = new Point(192, 61);
+        canvas.Location = new Point(182, 61);
         canvas.Name = "canvas";
         canvas.Size = new Size(49, 52);
         canvas.TabIndex = 5;
         // 
-        // Form1
+        // groupBox1
+        // 
+        groupBox1.Controls.Add(radioButton1);
+        groupBox1.Controls.Add(radioButton2);
+        groupBox1.Controls.Add(radioButton3);
+        groupBox1.Controls.Add(radioButton4);
+        groupBox1.Controls.Add(radioButton5);
+        groupBox1.Controls.Add(radioButton6);
+        groupBox1.Location = new Point(2, 9);
+        groupBox1.Name = "groupBox1";
+        groupBox1.Size = new Size(59, 168);
+        groupBox1.TabIndex = 6;
+        groupBox1.TabStop = false;
+        groupBox1.Text = "色系";
+        // 
+        // radioButton1
+        // 
+        radioButton1.AutoSize = true;
+        radioButton1.Checked = true;
+        radioButton1.Location = new Point(3, 19);
+        radioButton1.Name = "radioButton1";
+        radioButton1.Size = new Size(49, 19);
+        radioButton1.TabIndex = 0;
+        radioButton1.TabStop = true;
+        radioButton1.Text = "紅橙";
+        radioButton1.UseVisualStyleBackColor = true;
+        radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+        // 
+        // radioButton2
+        // 
+        radioButton2.AutoSize = true;
+        radioButton2.Location = new Point(3, 44);
+        radioButton2.Name = "radioButton2";
+        radioButton2.Size = new Size(37, 19);
+        radioButton2.TabIndex = 0;
+        radioButton2.Text = "黃";
+        radioButton2.UseVisualStyleBackColor = true;
+        radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+        // 
+        // radioButton3
+        // 
+        radioButton3.AutoSize = true;
+        radioButton3.Location = new Point(3, 69);
+        radioButton3.Name = "radioButton3";
+        radioButton3.Size = new Size(37, 19);
+        radioButton3.TabIndex = 0;
+        radioButton3.TabStop = true;
+        radioButton3.Text = "綠";
+        radioButton3.UseVisualStyleBackColor = true;
+        radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+        // 
+        // radioButton4
+        // 
+        radioButton4.AutoSize = true;
+        radioButton4.Location = new Point(3, 94);
+        radioButton4.Name = "radioButton4";
+        radioButton4.Size = new Size(37, 19);
+        radioButton4.TabIndex = 0;
+        radioButton4.TabStop = true;
+        radioButton4.Text = "藍";
+        radioButton4.UseVisualStyleBackColor = true;
+        radioButton4.CheckedChanged += radioButton4_CheckedChanged;
+        // 
+        // radioButton5
+        // 
+        radioButton5.AutoSize = true;
+        radioButton5.Location = new Point(3, 119);
+        radioButton5.Name = "radioButton5";
+        radioButton5.Size = new Size(37, 19);
+        radioButton5.TabIndex = 0;
+        radioButton5.Text = "紫";
+        radioButton5.UseVisualStyleBackColor = true;
+        radioButton5.CheckedChanged += radioButton5_CheckedChanged;
+        // 
+        // radioButton6
+        // 
+        radioButton6.AutoSize = true;
+        radioButton6.Location = new Point(3, 144);
+        radioButton6.Name = "radioButton6";
+        radioButton6.Size = new Size(37, 19);
+        radioButton6.TabIndex = 0;
+        radioButton6.TabStop = true;
+        radioButton6.Text = "粉";
+        radioButton6.UseVisualStyleBackColor = true;
+        radioButton6.CheckedChanged += radioButton6_CheckedChanged;
+        // 
+        // RandomColor
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(253, 176);
+        Controls.Add(groupBox1);
         Controls.Add(canvas);
         Controls.Add(resultLabel);
         Controls.Add(middleGrayButton);
@@ -105,6 +200,8 @@ partial class RandomColor
         Controls.Add(buttonTransfer);
         Name = "RandomColor";
         Text = "RandomColor";
+        groupBox1.ResumeLayout(false);
+        groupBox1.PerformLayout();
         ResumeLayout(false);
         PerformLayout();
     }
@@ -117,4 +214,11 @@ partial class RandomColor
     private Button middleGrayButton;
     private Label resultLabel;
     private Panel canvas;
+    private GroupBox groupBox1;
+    private RadioButton radioButton2;
+    private RadioButton radioButton1;
+    private RadioButton radioButton3;
+    private RadioButton radioButton6;
+    private RadioButton radioButton5;
+    private RadioButton radioButton4;
 }
--- RandomColor_ByAlpha/RandomColor.cs
+++ RandomColor_ByAlpha/RandomColor.cs
@@ -1,32 +1,29 @@
-using System.Reflection.Emit;
-
 namespace RandomColor_ByBrightness;
 
 public partial class RandomColor : Form
 {
-    private static readonly Random random = new Random();
-    private double _brightness;
+    private static readonly Random random = new();
     private const double _tolerance = 0; // 亮度容忍範圍
-    private int R, G, B;
+    private float H, S, B; // 色相 (Hue)、飽和度 (Saturation)、亮度 (Brightness)
+    private int minH, maxH;
 
     public RandomColor()
     {
+        minH = 0;
+        maxH = 45;
         InitializeComponent();
     }
 
     // KeyPress 限制輸入字元
     private void alphaTextBox_KeyPress(object sender, KeyPressEventArgs e)
     {
-        if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-        {
-            e.Handled = true; // 阻止非數字輸入
-        }
+        if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) e.Handled = true; // 阻止非數字輸入
     }
 
     // TextChanged 確保內容合法
     private void alphaTextBox_TextChanged(object sender, EventArgs e)
     {
-        if (int.TryParse(alphaTextBox.Text, out int value))
+        if (int.TryParse(alphaTextBox.Text, out var value))
         {
             if (value >= 256)
             {
@@ -42,48 +39,153 @@ public partial class RandomColor : Form
 
     private void middleGrayButton_Click(object sender, EventArgs e)
     {
-        alphaTextBox.Text = "128";
-        int i = int.Parse(alphaTextBox.Text);
-        GetRGB(i);
-
+        alphaTextBox.Text = "128"; // 設定目標亮度為128
+        var targetBrightness = float.Parse(alphaTextBox.Text) / 255f;
+        GetHSB(targetBrightness);
     }
 
     private void buttonTransfer_Click(object sender, EventArgs e)
     {
-        int i = int.Parse(alphaTextBox.Text);
-        GetRGB(i);
+        var targetBrightness = float.Parse(alphaTextBox.Text) / 255f;
+        GetHSB(targetBrightness);
     }
 
-    public void GetRGB(double _targetBrightness)
+    // 根據指定的亮度調整顏色
+    public void GetHSB(float targetBrightness)
     {
+        Color color;
+
         do
         {
-            // 隨機生成 R、G、B 值
-            R = random.Next(0, 256);
-            G = random.Next(0, 256);
-            B = random.Next(0, 256);
+            // 隨機生成 HSB 值
+            H = random.Next(minH, maxH); // 色相 (0-360°)
+            S = (float)random.NextDouble(); // 飽和度 (0.0 - 1.0)
+            B = (float)random.NextDouble(); // 亮度 (0.0 - 1.0)
 
-            // 計算亮度
-            _brightness = 0.299 * R + 0.587 * G + 0.114 * B;
-        } while (_brightness < _targetBrightness - _tolerance || _brightness > _targetBrightness + _tolerance);
+            // 將 HSB 轉換為 RGB
+            color = HSBtoRGB(H, S, B);
+        } while (Math.Abs(CalculateBrightness(color) - targetBrightness) > _tolerance); // 確保亮度在容忍範圍內
 
         // 顯示結果
-        Console.WriteLine($"Generated RGB: R={R}, G={G}, B={B}, Brightness={_brightness}");
-        resultLabel.Text = $"R={R}, G={G}, B={B}, Brightness={Math.Round(_brightness, 1)}";
+        Console.WriteLine($"Generated HSB: H={H:F1}, S={S:F2}, B={B:F2}");
+        Console.WriteLine($"Converted RGB: R={color.R}, G={color.G}, B={color.B}");
 
-        canvas.BackColor = Color.FromArgb(R, G, B);
+        resultLabel.Text = $"H={Math.Round(H, 1)}, S={Math.Round(S * 100, 1)}%, B={Math.Round(B * 100, 1)}%\n" +
+                           $"R={color.R}, G={color.G}, B={color.B}";
+
+        canvas.BackColor = color;
+
         // 創建 Bitmap 並繪製顏色
-        using (Bitmap bmp = new Bitmap(canvas.Width, canvas.Height))
+        using (var bmp = new Bitmap(canvas.Width, canvas.Height))
         {
-            using (Graphics g = Graphics.FromImage(bmp))
+            using (var g = Graphics.FromImage(bmp))
             {
                 g.Clear(canvas.BackColor); // 填充 Panel 的背景顏色
             }
 
             // 複製圖片到剪貼簿
             Clipboard.SetImage(bmp);
-            //MessageBox.Show("顏色圖片已複製到剪貼簿！");
         }
     }
 
+    // HSB 轉 RGB 方法
+    public static Color HSBtoRGB(float h, float s, float b)
+    {
+        float r = 0, g = 0, blue = 0;
+
+        if (s == 0)
+        {
+            // 灰階顏色（無色相）
+            r = g = blue = b;
+        }
+        else
+        {
+            var sectorPos = h / 60f;
+            var sectorNumber = (int)Math.Floor(sectorPos); // 所在的色相區段
+            var fractionalSector = sectorPos - sectorNumber;
+
+            var p = b * (1 - s);
+            var q = b * (1 - s * fractionalSector);
+            var t = b * (1 - s * (1 - fractionalSector));
+
+            switch (sectorNumber)
+            {
+                case 0:
+                    r = b;
+                    g = t;
+                    blue = p;
+                    break;
+                case 1:
+                    r = q;
+                    g = b;
+                    blue = p;
+                    break;
+                case 2:
+                    r = p;
+                    g = b;
+                    blue = t;
+                    break;
+                case 3:
+                    r = p;
+                    g = q;
+                    blue = b;
+                    break;
+                case 4:
+                    r = t;
+                    g = p;
+                    blue = b;
+                    break;
+                case 5:
+                    r = b;
+                    g = p;
+                    blue = q;
+                    break;
+            }
+        }
+
+        // 將結果轉換為 0-255 的範圍
+        return Color.FromArgb((int)(r * 255), (int)(g * 255), (int)(blue * 255));
+    }
+
+    // 計算 RGB 顏色的亮度（輝度）
+    public static float CalculateBrightness(Color color)
+    {
+        return 0.299f * color.R / 255 + 0.587f * color.G / 255 + 0.114f * color.B / 255;
+    }
+
+    private void radioButton1_CheckedChanged(object sender, EventArgs e)
+    {
+        minH = 0;
+        maxH = 45;
+    }
+
+    private void radioButton2_CheckedChanged(object sender, EventArgs e)
+    {
+        minH = 45;
+        maxH = 75;
+    }
+
+    private void radioButton3_CheckedChanged(object sender, EventArgs e)
+    {
+        minH = 75;
+        maxH = 150;
+    }
+
+    private void radioButton4_CheckedChanged(object sender, EventArgs e)
+    {
+        minH = 150;
+        maxH = 270;
+    }
+
+    private void radioButton5_CheckedChanged(object sender, EventArgs e)
+    {
+        minH = 270;
+        maxH = 330;
+    }
+
+    private void radioButton6_CheckedChanged(object sender, EventArgs e)
+    {
+        minH = 330;
+        maxH = 345;
+    }
 }
